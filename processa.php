56ba5346e7a7993ab3396d5e60c7a7debebf6cde
Rui de Alencar Frota <ruifrota@09569L.pge.ce.gov.br>
2018-10-10 16:31:05 -0300

    first commit

---
+++ processa.php
@@ -0,0 +1,25 @@
+<?php
+
+
+
+$nome = $_POST["nome"];
+
+$idade = $_POST["idade"];
+
+$sexo = $_POST["sexo"];
+
+$estuda = $_POST["estuda"];
+
+
+
+?>
+
+
+
+Nome: <?php echo $nome." //"; ?>
+
+Idade: <?php echo $idade." //"; ?>
+
+Sexo: <?php echo $sexo;" //"; ?>
+
+Estuda: <?php echo $estuda. ?>
